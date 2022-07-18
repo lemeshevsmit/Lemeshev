4f768dc42c0ded9abda3f5a6889a5dac057ebbc8
Aleksandr Lemeshev <lemeshevolex@gmail.com>
2022-07-18 18:01:09 +0300

    add logic and test

--- src/com/shpp/p2p/cs/olemeshev/assignment10/Assignment10Part1.java
+++ src/com/shpp/p2p/cs/olemeshev/assignment10/Assignment10Part1.java
@@ -1,67 +1,135 @@
 package com.shpp.p2p.cs.olemeshev.assignment10;
 
-import java.util.HashMap;
-import java.util.LinkedList;
+import java.util.*;
 
 public class Assignment10Part1 {
+    private static final String[] OPERATORS = new String[]{"^", "/", "*", "-", "+"};
+
     public static void main(String[] args) {
+        if (args == null || Arrays.equals(args, new String[]{}) || args[0].equals("")) {
+            System.out.println("Formula is null or empty: " + Arrays.toString(args));
+            return;
+        }
         String startFormula = args[0];
-        HashMap<String, Double> parameters = getParameters(args);
-
-        System.out.println(calculate(startFormula, parameters));
-
-//        for (Map.Entry<String, Double> entry : parameters.entrySet()) {
-//            System.out.println(entry.getKey() + " " + entry.getValue());
-//        }
+        HashMap<String, String> parameters = getParameters(args);
+        try {
+            System.out.println(calculate(startFormula, parameters));
+        } catch (NullPointerException e) {
+            return;
+        } catch (ArithmeticException e) {
+            System.out.println("Incorrect operators: " + e.getMessage());
+        } catch (NumberFormatException e) {
+            System.out.println("Incorrect formula: " + e.getMessage());
+        }
     }
 
-    static double calculate(String line, HashMap<String, Double> variables) {
-        String[] formula = line.split(" ");
-        for (int i = 0; i < formula.length; i++) {
-            for (String kay : variables.keySet()) {
-                if (formula[i].equals(kay)) {
-                    formula[i] = String.valueOf(variables.get(kay));
+    static double calculate(String line, HashMap<String, String> variables) throws NullPointerException {
+        String[] formula = getFormula(line, variables);
+        for (String operator : OPERATORS) {
+            formula = findOperation(formula, operator);
+            if (formula == null) throw new NullPointerException();
+        }
+        if (formula.length > 1) throw new ArithmeticException(formula[1]);
+        else return Double.parseDouble(formula[0]);
+    }
+
+    /**
+     * This method create final view of formula without parameters
+     *
+     * @param line      start formula
+     * @param variables list with parameters
+     * @return final formula
+     */
+    private static String[] getFormula(String line, HashMap<String, String> variables) {
+        String[] formula = line.trim().split(" ");
+        if (variables.size() > 0) {
+            LinkedList<String> newFormula = new LinkedList<>();
+            for (String s : formula) {
+                newFormula.add(s);
+                for (String kay : variables.keySet()) {
+                    if (s.equals(kay)) {
+                        newFormula.removeLast();
+                        String[] values = variables.get(kay).split(" ");
+                        newFormula.addAll(Arrays.asList(values));
+                    }
                 }
             }
+            formula = newFormula.toArray(new String[0]);
         }
-        return findValue(formula);
+        return formula;
     }
 
-    private static double findValue(String[] formula) {
-        findOperation(formula, "*");
-
-        return 0.0;
-    }
-
-    private static void findOperation(String[] formula, String o) {
+    private static String[] findOperation(String[] formula, String o) {
         LinkedList<String> temp = new LinkedList<>();
         for (int i = 0; i < formula.length; i++) {
             if (formula[i].equals(o)) {
-                temp.removeLast();
-                double value = Double.parseDouble(formula[i - 1]) *
-                        Double.parseDouble(formula[i + 1]);
-                temp.add(Double.toString(value));
-            }
-            temp.add(formula[i]);
+                try {
+                    double value1 = Double.parseDouble(temp.getLast());
+                    double value2 = Double.parseDouble(formula[i + 1]);
+                    temp.removeLast();
+                    temp.add(Double.toString(operator(value1, value2, o)));
+                    i++;
+                } catch (NumberFormatException e) {
+                    System.out.println("Unknown value in formula: " + e.getMessage());
+                    return null;
+                }
+            } else temp.add(formula[i]);
         }
+        return temp.toArray(new String[0]);
     }
 
-    static HashMap<String, Double> getParameters(String[] parameters) {
-        HashMap<String, Double> variables = new HashMap<>();
+    /**
+     * This method do arithmetical operation with two double variable
+     *
+     * @param value1 first value
+     * @param value2 second value
+     * @param o      String association of math operation
+     * @return result of arithmetical operation
+     */
+    static double operator(double value1, double value2, String o) {
+        switch (o) {
+            case "+" -> {
+                return value1 + value2;
+            }
+            case "-" -> {
+                return value1 - value2;
+            }
+            case "*" -> {
+                return value1 * value2;
+            }
+            case "/" -> {
+                return value1 / value2;
+            }
+            case "^" -> {
+                return Math.pow(value1, value2);
+            }
+        }
+        return 0.0;
+    }
+
+
+    /**
+     * This method return list with input parameters. If we
+     * don't input parameters return empty list.
+     *
+     * @param parameters parameters from console
+     * @return list with all input parameters
+     */
+    static HashMap<String, String> getParameters(String[] parameters) {
+        HashMap<String, String> list = new HashMap<>();
         if (parameters.length > 1) {
             for (int i = 1; i < parameters.length; i++) {
-                String[] line = parameters[i].split("=");
-                if (!variables.containsKey(line[0])) {
+                String[] value = parameters[i].split(" = ");
+                if (!list.containsKey(value[0])) {
                     try {
-                        Double value = Double.valueOf(line[1]);
-                        variables.put(line[0], value);
-                    } catch (NumberFormatException e) {
-                        System.out.println("Incorrect parameters value\n");
-                        e.printStackTrace();
+                        list.put(value[0], value[1]);
+                    } catch (ArrayIndexOutOfBoundsException e) {
+                        System.out.println("Incorrect parameter: " +
+                                Arrays.toString(value));
                     }
                 }
             }
         }
-        return variables;
+        return list;
     }
 }
---
+++ src/com/shpp/p2p/cs/olemeshev/assignment10/Test.java
@@ -0,0 +1,113 @@
+package com.shpp.p2p.cs.olemeshev.assignment10;
+
+import java.io.BufferedOutputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+public class Test {
+    public static void main(String[] args) throws FileNotFoundException {
+        String[][] correctTests = new String[][]{
+                {"1 + a * 2", "a = 2"},
+                {"2 * 4 - 6 / 3 + 6 + 1 / 3 - 6 + -1 * -1"},
+                {"1 + 3 + 5"},
+                {"1 - 3 - 5"},
+                {"1 * 3 * 5"},
+
+                {"-1 * -3 * -5"},
+                {"-1"},
+                {"4 / 2 * 2 + 2 ^ 3 + 3 - -1 - 3 + -5 * 2 + 10"},
+                {"5 * 4 / 2"},
+                {"5 / 4 * 2"},
+
+                {"3 ^ -1"},
+                {"a ^ b + a * 2 - 4 * c", "a = 2", "b = 3", "c = 1"},
+                {"a ^ b / a * 2 - 4 * c * a", "a = 2", "b = 3", "x = 0", "c = 1"},
+                {"1 + a * 2", "a = -2.0D"},
+                {"1 + a * 2", "a = 2 * 3 ^ 2"}
+        };
+        String[] correctAnswers = new String[]{
+                "5.0", String.valueOf(7 + 1.0 / 3), "9.0", "-7.0", "15.0",
+                "-15.0", "-1.0", "13.0", "10.0", "2.5",
+                String.valueOf(Math.pow(3, -1)), "8.0", "0.0", "-3.0", "37.0"
+        };
+
+        String[][] incorrectTests = new String[][]{
+                {},
+                null,
+                {""},
+
+                {"1 + a* 2", "a = 2"},
+                {"1 + a & 2", "a = 2"},
+                {"1+a&2", "a = 2"},
+
+                {"1 + a * 2", "b = 2"},
+                {" a ^ b + a * 2 - 4 * c ", "a = 2", "b = 3 ", "c = 1"},
+                {"1 + a * 2", "t =tr"}
+        };
+        String[] incorrectAnswers = new String[]{
+                "Formula is null or empty: []",
+                "Formula is null or empty: null",
+                "Formula is null or empty: []",
+
+                "Unknown value in formula: For input string: \"a*\"",
+                "Incorrect operators: &",
+                "Incorrect formula: For input string: \"1+a&2\"",
+
+                "Unknown value in formula: For input string: \"a\"",
+                "8.0",
+                ""
+        };
+
+        testing(correctTests, correctAnswers);
+        testing(incorrectTests, incorrectAnswers);
+
+
+        //start();
+    }
+
+    static void start() {
+        String[] test = new String[]{"1 + a * 2", "t =tr"};
+        Assignment10Part1.main(test);
+    }
+
+    /**
+     * It's test method.
+     *
+     * @param tests   arrays with tests
+     * @param answers array with answers
+     */
+    private static void testing(String[][] tests, String[] answers) throws FileNotFoundException {
+        // запоминаю старую консоль
+        PrintStream out = System.out;
+        // создаю поток для записи в файл
+        FileOutputStream file = new FileOutputStream("result.txt");
+        //создаю поток
+        BufferedOutputStream buffer = new BufferedOutputStream(file);
+        // создаю консоль для вывода
+        PrintStream console = new PrintStream(buffer, true);
+        // меняю консоль на новую для вывода в файл.
+        System.setOut(console);
+        for (String[] test : tests) {
+            Assignment10Part1.main(test);
+        }
+        System.setOut(out);
+
+        FileOpener fileOpener = new FileOpener();
+        ArrayList<String> result = fileOpener.openFile("result.txt");
+        for (int i = 0; i < result.size(); i++) {
+            String value = result.get(i);
+            if (value.equals(answers[i])) {
+                System.out.println("Test: " + Arrays.toString(tests[i]) + " PASSED!");
+                System.out.println("Result: " + value);
+            } else {
+                System.out.println("Test: " + Arrays.toString(tests[i]) + " CRUSH!");
+                System.out.println("You output: " + value);
+                System.out.println("Expected: " + answers[i]);
+            }
+        }
+    }
+}
